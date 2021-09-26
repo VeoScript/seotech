3fedba36c5d7669a2829e32663b71bcca44c6ce7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-26 14:05:46 +0800

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import React from 'react'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>SEO Tech</title>
+      </Head>
+      <div className="flex w-full h-screen font-poppins">
+        <h1>SEO TECH</h1>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Home
