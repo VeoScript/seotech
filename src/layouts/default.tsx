ea83193d355c1f65382f3fa550bfddabd8a06fd1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-27 14:19:07 +0800

    chore: update default layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from '~/components/NavBar'
+import Footer from '~/components/Footer'
 
 interface IProps {
   children: any
@@ -7,10 +8,13 @@ interface IProps {
 
 const Layout: React.FC<IProps> = ({ children }) => {
   return (
-    <div className="font-montserrat flex flex-col w-full h-screen overflow-y-hidden">
+    <div className="font-montserrat flex flex-col w-full h-screen overflow-y-hidden cursor-default">
       <NavBar />
-      <div className="flex flex-col w-full h-full overflow-y-auto">
-        {children}
+      <div className="relative flex flex-col w-full h-full overflow-y-auto">
+        <div className="flex flex-col w-full h-full">
+          {children}
+        </div>
+        <Footer />
       </div>
     </div>
   )
