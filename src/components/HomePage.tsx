e24da03d0c37d12c64e705a950dacdf3e387b260
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-27 11:26:38 +0800

    feat: add framer-motion (#2)

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Link from 'next/link'
+import { FacebookIcon, TwitterIcon } from '~/utils/Icons'
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center w-full max-w-2xl space-y-5">
+      <div className="flex w-full">
+        <h1 className="font-bold text-5xl">
+          We make <br /> digital beautiful.
+          <hr className="mt-5 border-b-2 border-blue-500 w-full max-w-[5rem]" />
+        </h1>
+      </div>
+      <div className="flex w-full">
+        <h6 className="font-normal text-sm">
+          These are the words we live by in everything we do. Every story we tell,
+          every brand we build, and every interaction we create must not only look beautiful but has to feel and function beautifully, too.
+          Crafted to perfection.
+        </h6>
+      </div>
+      <div className="flex flex-col w-full">
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Search Engine Optimization</h6>
+        </div>
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Bootstrap Latest Version</h6>
+        </div>
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Tailwind CSS</h6>
+        </div>
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Clean & Modern Designs</h6>
+        </div>
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Responsive Web Designs</h6>
+        </div>
+        <div>
+          <h6 className="font-bold text-base text-gray-700">&gt; Google Fonts Used</h6>
+        </div>
+      </div>
+      <div className="flex flex-col w-full space-y-3">
+        <h6 className="font-normal text-sm">
+          We create <span className="italic">Social Media</span>, <span className="italic">Web Application</span>, and <span className="italic">Desktop Application</span>. 
+          Visit our <Link href="/"><a className="text-blue-500 hover:underline">portfolio</a></Link> to see our latest designs.
+        </h6>
+        <div className="flex items-center space-x-1">
+          <span className="text-xs">Follow us on</span>
+          <FacebookIcon className="w-3.5 h-3.5 fill-current text-gray-800" />
+          <TwitterIcon className="w-4 h-4 fill-current text-gray-800" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
